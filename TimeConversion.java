841e9239dab8b74b9a252c1d6022fafd1898eb66
DeTrorC <140917289+DeTrorC@users.noreply.github.com>
2023-10-30 22:26:04 +0530

    Update TimeConversion.java

--- TimeConversion.java
+++ TimeConversion.java
@@ -1,78 +1,76 @@
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
-import javafx.stage.Stage;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Arrays;
 
-public class TimeZoneConversionGUI extends Application {
+public class TimeConversion {
 
     public static void main(String[] args) {
-        launch(args);
-    }
+        Scanner scanner = new Scanner(System.in);
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Time Zone Conversion Tool");
-
-        // Create a VBox to hold the UI components
-        VBox vbox = new VBox(10);
-        vbox.setPadding(new Insets(10));
-        
-        // Create labels and dropdowns for time zone selection
-        Label localTimeZoneLabel = new Label("Select your local time zone:");
-        ComboBox<String> localTimeZoneComboBox = new ComboBox<>();
-        
-        Label targetTimeZoneLabel = new Label("Select the target time zone:");
-        ComboBox<String> targetTimeZoneComboBox = new ComboBox<>();
+        System.out.println("Time Zone Conversion Tool");
+        System.out.println("=========================");
 
         // List of time zones (you can extend this list)
         String[] allTimeZones = {
             "America/New_York",
             "America/Los_Angeles",
             "Europe/London",
-            // Add more time zones here
+            "Europe/Paris",
+            "Asia/Tokyo",
+            "Australia/Sydney",
+            "Africa/Cairo",
+            "Asia/Dubai",
+            "Asia/Kolkata",
+            "Asia/Shanghai",
+            "Europe/Berlin",
+            "Europe/Rome",
+            "America/Chicago",
+            "America/Toronto",
+            "America/Mexico_City",
+            "Pacific/Honolulu",
+            "Australia/Melbourne",
+            "Asia/Singapore",
+            "Asia/Hong_Kong",
+            "Europe/Madrid",
+            "Africa/Johannesburg",
+            "Europe/Amsterdam",
+            "America/Phoenix",
+            "Asia/Bangkok",
+            "America/Buenos_Aires",
+            "Pacific/Auckland",
+            "Europe/Stockholm",
+            "America/Denver",
+            "America/Anchorage",
+            "Asia/Seoul",
+            "Africa/Lagos"
         };
 
-        localTimeZoneComboBox.getItems().addAll(allTimeZones);
-        targetTimeZoneComboBox.getItems().addAll(allTimeZones);
+        System.out.println("Common Time Zones:");
+        for (int i = 0; i < allTimeZones.length; i++) {
+            System.out.println((i + 1) + ". " + allTimeZones[i]);
+        }
+        System.out.print("Enter the number of your local time zone: ");
+        int userLocalTimeZoneIndex = scanner.nextInt();
+        String userLocalTimeZone = allTimeZones[userLocalTimeZoneIndex - 1];
 
-        // Create labels to display results
-        Label localTimeLabel = new Label("Your Local Time:");
-        Label localTimeResult = new Label();
+        System.out.print("Enter the number of the target time zone: ");
+        int targetTimeZoneIndex = scanner.nextInt();
+        String targetTimeZone = allTimeZones[targetTimeZoneIndex - 1];
 
-        Label targetTimeLabel = new Label("Time in Target Zone:");
-        Label targetTimeResult = new Label();
+        ZoneId userLocalZone = ZoneId.of(userLocalTimeZone);
+        ZoneId targetZone = ZoneId.of(targetTimeZone);
 
-        // Create a Convert button
-        Button convertButton = new Button("Convert");
-        convertButton.setOnAction(e -> {
-            String userLocalTimeZone = localTimeZoneComboBox.getValue();
-            String targetTimeZone = targetTimeZoneComboBox.getValue();
+        ZonedDateTime userLocalTime = ZonedDateTime.now(userLocalZone);
+        ZonedDateTime targetTime = userLocalTime.withZoneSameInstant(targetZone);
 
-            ZoneId userLocalZone = ZoneId.of(userLocalTimeZone);
-            ZoneId targetZone = ZoneId.of(targetTimeZone);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
 
-            ZonedDateTime userLocalTime = ZonedDateTime.now(userLocalZone);
-            ZonedDateTime targetTime = userLocalTime.withZoneSameInstant(targetZone);
+        System.out.println("\nYour Local Time (" + userLocalTimeZone + "): " + userLocalTime.format(formatter));
+        System.out.println("Time in " + targetTimeZone + ": " + targetTime.format(formatter));
 
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
-
-            localTimeResult.setText(userLocalTime.format(formatter));
-            targetTimeResult.setText(targetTime.format(formatter));
-        });
-
-        // Add components to the VBox
-        vbox.getChildren().addAll(localTimeZoneLabel, localTimeZoneComboBox, targetTimeZoneLabel, targetTimeZoneComboBox, convertButton, localTimeLabel, localTimeResult, targetTimeLabel, targetTimeResult);
-
-        // Create a scene and set it on the stage
-        Scene scene = new Scene(vbox, 400, 300);
-        primaryStage.setScene(scene);
-
-        // Show the stage
-        primaryStage.show();
+        scanner.close();
     }
 }
